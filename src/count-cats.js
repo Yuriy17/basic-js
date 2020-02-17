d6537e8e2d590feadfa4dac15e131a7d0bb2aee9
Yuriy17 <yuriy_sh@zoho.com>
2020-02-17 17:11:46 +0300

    feat: resolve 3 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(matrix) {
+  let cats = 0;
+  if (matrix.length !== 0) {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let y = 0; y < matrix[i].length; y++) {
+        if (matrix[i][y] === "^^") {
+          cats += 1;
+        }
+      }
+    }
+  }
+  return cats;
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let res = "";
+  members.forEach(el => {
+    if (typeof el === "string") {
+      el = el.replace(/\s*/g, "");
+      res += el[0].toUpperCase();
+    }
+  });
+  return res
+    .split("")
+    .sort()
+    .join("");
   // remove line with error and write your code here
 };
