2817c3c47907306c12fbe4957d41232cc63c35c9
Yuriy17 <yuriy_sh@zoho.com>
2020-02-21 15:43:07 +0300

    feat: complete except one test with boolean types

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,23 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let res = "";
+  if (!options.repeatTimes) options.repeatTimes = 1;
+  /* if (typeof str === "boolean") str = str.toString(); */
+  for (let i = 0; i < options.repeatTimes; i++) {
+    res += str;
+    /*     options.addition =
+      typeof options.addition === "boolean"
+        ? options.addition.toString()
+        : options.addition; */
+    if (options.addition) {
+      if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+      for (let y = 0; y < options.additionRepeatTimes; y++) {
+        res += options.addition;
+        if (y !== options.additionRepeatTimes - 1)
+          res += options.additionSeparator;
+      }
+    }
+    if (!options.separator) options.separator = "+";
+    if (i !== options.repeatTimes - 1) res += options.separator;
+  }
+  return res;
 };
-  
